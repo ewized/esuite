a13741b1927ebd9a19a5bed0163155b72c3153f9
ewized <ewized@gmail.com>
2013-08-14 20:28:30 -0700

    Fix  not reseting back to defaults.

--- eprotect/src/main/java/net/year4000/eprotect/ProtectEvents.java
+++ eprotect/src/main/java/net/year4000/eprotect/ProtectEvents.java
@@ -10,6 +10,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -38,17 +39,14 @@ public class ProtectEvents implements Listener{
         if(action==Action.RIGHT_CLICK_BLOCK){
         	if(player.isSneaking()){
             	player.sendMessage(ChatColor.GRAY + "This block is protected by: " + protect.getSign(block));
-            	protect.message = "no one";
-            } else if(protect.isProtected(block, null)){
+            } else if(protect.isProtected(block, player) && !player.isSneaking()){
         		event.setCancelled(true);
-        		protect.result = false;
-        	} else if(protect.isProtected(block, player)){
-        		event.setCancelled(true);
-        		protect.result = false;
         	}
+        	protect.result = false;
+        	protect.message = "no one";
         }
     }
-
+	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onEntityExplode(EntityExplodeEvent event) {
         for(Block block : event.blockList()){
@@ -76,16 +74,21 @@ public class ProtectEvents implements Listener{
 
 		if(block.getType() == Material.WALL_SIGN || block.getType() == Material.SIGN_POST){
 	        if(event.getLine(0).equalsIgnoreCase("[Protect]")){
-	        	try {
-					CommandBook.inst().checkPermission(player, "eprotect.create.other");
-					event.setLine(0, "[Protect]");
-					if(event.getLine(1).equals("")){
-						event.setLine(1, player.getName());
+	        	if(!protect.isProtected(block, null)){
+		        	try {
+						CommandBook.inst().checkPermission(player, "eprotect.create.other");
+						event.setLine(0, "[Protect]");
+						if(event.getLine(1).equals("")){
+							event.setLine(1, player.getName());
+						}
+					} catch (CommandPermissionsException e) {
+			        	event.setLine(0, "[Protect]");
+			        	event.setLine(1, player.getName());
 					}
-				} catch (CommandPermissionsException e) {
-		        	event.setLine(0, "[Protect]");
-		        	event.setLine(1, player.getName());
-				}
+	        	}  else{
+	        		event.setCancelled(true);
+		        	player.sendMessage(ChatColor.GOLD + "This block is all ready protected.");
+		        }
 	        }
 		}
     }
