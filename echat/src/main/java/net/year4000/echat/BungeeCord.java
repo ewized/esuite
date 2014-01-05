7eb27267fd67a98c922f88a845420d8d207fcf07
Joshua Rodriguez <ewized@gmail.com>
2014-01-04 16:45:52 -0800

    Fixed EOF error.

--- echat/src/main/java/net/year4000/echat/BungeeCord.java
+++ echat/src/main/java/net/year4000/echat/BungeeCord.java
@@ -83,16 +83,17 @@ public class BungeeCord implements PluginMessageListener {
             byte[] msgbytes = new byte[len];
             in.readFully(msgbytes);
             DataInputStream msgin = new DataInputStream(new ByteArrayInputStream(msgbytes));
-            long sentTime = msgin.readLong(); // Get sent time
-
-            // Only run if message was sent with in 1 second
-            long currentTime = System.currentTimeMillis();
-            if (sentTime + 1000 < currentTime) {
-                return;
-            }
 
             // Only get data from eChat
             if (subchannel.equals("eChat")) {
+                long sentTime = msgin.readLong(); // Get sent time
+
+                // Only run if message was sent with in 1 second
+                long currentTime = System.currentTimeMillis();
+                if (sentTime + 1000 < currentTime) {
+                    return;
+                }
+
                 // Set up the environment.
                 Message message = new Message();
                 message.setPlayerName(msgin.readUTF());
